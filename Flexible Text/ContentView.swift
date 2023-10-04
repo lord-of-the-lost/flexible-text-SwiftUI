ecc84401be022de0b6e4a9ce44ac0ed11a6ab0c4
lord-of-the-lost <ignatov.99@yandex.ru>
2023-10-04 14:33:15 +0300

    add flexible text into container

--- Flexible Text/ContentView.swift
+++ Flexible Text/ContentView.swift
@@ -8,14 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var containerWidth: CGFloat = 300
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            VStack {
+                Text("Марафон ")
+                    .fontWeight(.regular)
+                    .foregroundColor(.gray)
+                + Text("по SwiftUI ")
+                    .fontWeight(.regular)
+                + Text("«Отцовский пинок»")
+                    .bold()
+                    .font(.title)
+                    .foregroundColor(.blue)
+            }
+            .frame(width: containerWidth)
+            .border(.red)
+            .multilineTextAlignment(.leading)
+            
+            Slider(value: $containerWidth,
+                   in: 100...300,
+                   step: 1)
+            .padding()
         }
-        .padding()
     }
 }
 
