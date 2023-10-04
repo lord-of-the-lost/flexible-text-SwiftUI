0a60d6c8b1d3ffb781405054e6d5a997af296d62
lord-of-the-lost <ignatov.99@yandex.ru>
2023-10-04 13:15:59 +0300

    Initial Commit

---
+++ Flexible Text/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Flexible Text
+//
+//  Created by Николай Игнатов on 04.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Flexible Text/Flexible_TextApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flexible_TextApp.swift
+//  Flexible Text
+//
+//  Created by Николай Игнатов on 04.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct Flexible_TextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
